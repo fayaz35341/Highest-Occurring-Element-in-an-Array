0432504910c1cf5017ad84b51f9ba27f208d642b
Riyaz Shaik <94755295+fayaz35341@users.noreply.github.com>
2025-11-26 23:04:21 +0530

    Implement mostFrequentElement function in Solution class

---
+++ Highest-Occurring-Element-in-an-Array.js
@@ -0,0 +1,18 @@
+class Solution {
+    mostFrequentElement(nums) {
+        let freq = {};
+        for (let i of nums) {
+            freq[i] = (freq[i] || 0) + 1;
+        }
+        let max_freq = Math.max(...Object.values(freq));
+        let a = [];
+        for (let i in freq) {
+            if (freq[i] === max_freq) {
+                a.push(Number(i));
+            }
+        }
+        return a;
+    }
+}
+const n = [1, 2, 2, 1, 3];
+console.log(new Solution().mostFrequentElement(n));
